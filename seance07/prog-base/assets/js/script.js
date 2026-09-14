9ec54219002786b2a043a394ebfb960c66703f14
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2026-09-14 14:45:26 -0400

    Séance 7 : diffusion.

--- seance07/prog-base/assets/js/script.js
+++ seance07/prog-base/assets/js/script.js
@@ -2,10 +2,8 @@
 const canvas = document.querySelector("canvas");
 const contexte = canvas.getContext("2d");
 const imageLuciole = new Image();
-const imageJoueur = new Image();
 
 imageLuciole.src = "assets/images/luciole.svg";
-imageJoueur.src = "assets/images/joueur.svg";
 
 let imagesChargees = 0;
 
@@ -19,14 +17,14 @@ function dessinerScene() {
   contexte.font = "28px sans-serif";
   contexte.fillText("Trouvez la luciole !", 30, 45);
 
-  contexte.drawImage(imageJoueur, 100, 220, 56, 56);
   contexte.drawImage(imageLuciole, 600, 180, 40, 40);
 }
 
 function imageEstChargee() {
   imagesChargees++;
-  if (imagesChargees === 2) dessinerScene();
+  if (imagesChargees === 1) {
+    dessinerScene();
+  }
 }
 
 imageLuciole.addEventListener("load", imageEstChargee);
-imageJoueur.addEventListener("load", imageEstChargee);
